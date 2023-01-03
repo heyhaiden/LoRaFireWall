a63563c06d4b3edaf3a27858a98bdc0a816f2bcb
Haiden McGill <46386953+heyhaiden@users.noreply.github.com>
2023-01-03 13:15:53 +0100

    Updated receiver script

---
+++ code/LoRa_Receiver/fireAware_Receiver.ino
@@ -0,0 +1,75 @@
+/*
+  FireAware: RECEIVER BEACON
+  v2.0 
+  3 January 2023
+*/
+
+#include <MKRWAN.h>
+#include <LoRa.h>
+#include <Adafruit_NeoPixel.h>
+#include <ArduinoLowPower.h>
+
+// Set up the MKR WAN 1310
+LoRaModem modem;
+
+#define DHTTYPE DHT22  
+#define PIN 3
+#define NUMPIXELS 8
+
+Adafruit_NeoPixel pixels(NUMPIXELS, PIN);
+
+void setup() {
+  Serial.begin(9600);
+  while (!Serial)
+    ;
+
+  Serial.println("Initializing GONDOR Receiver");
+
+  if (!LoRa.begin(868E6)) {
+    Serial.println("LoRa Radio connection failed!");
+    while (1);
+  }
+
+  Serial.println("Waiting for packets...");
+
+  pixels.begin();
+
+  delay(5000);
+}
+
+void loop() {
+  
+  parsePacket();
+  
+}
+
+void parsePacket() {
+
+  // Check if a packet has been received
+  int packetSize = modem.parsePacket();
+
+  if (packetSize) {
+    // Read the data from the packet
+    String receivedData;
+    while (modem.available()) {
+      receivedData += (char)modem.read();
+    }
+
+    // Check if the received data contains the string "fire"
+    if (strstr(receivedData.c_str(), "FLAME")) {
+
+      // Turn on the NeoPixel strip
+      for (int i = 0; i < NUMPIXELS; i++) {
+        pixels.setPixelColor(i, pixels.Color(0, 20, 0));
+      }
+      pixels.show();
+
+      delay(5000);
+    }
+  } 
+  
+  else {
+    // Put the device in deep sleep until a packet is received
+    LowPower.sleep();
+  }
+}
